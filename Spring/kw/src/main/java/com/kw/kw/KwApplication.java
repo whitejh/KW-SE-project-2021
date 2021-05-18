2a7aad2119a091180701af807e3c818d6a1d1268
arsgsg1 <arsgsg1@gmail.com>
2021-05-18 23:16:46 +0900

    [ADD] Innitial Commit
    스프링 초기 설정 및 의존 라이브러리 추가

---
+++ Spring/kw/src/main/java/com/kw/kw/KwApplication.java
@@ -0,0 +1,13 @@
+package com.kw.kw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwApplication.class, args);
+	}
+
+}
