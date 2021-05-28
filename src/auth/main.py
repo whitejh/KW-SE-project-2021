e1a57e552e75d78b55146f5ba0a74d6c23c404ed
Jung Dong Ho <dongho971220@gmail.com>
2021-05-28 18:03:37 +0900

    role, hashed_pw 추가 / reformat codes

--- src/auth/main.py
+++ src/auth/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Response
 
 app = FastAPI()
 
+
 @app.get("/cookie")
 def create_cookie(response: Response):
     response.set_cookie(key="fakesession", value="fake-cookie-session-value")
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,30 +1,35 @@
+CREATE TYPE public.role AS ENUM
+    ('ADMIN', 'BUYER', 'SELLER');
+
 CREATE TABLE IF NOT EXISTS public.member
 (
-    id bigserial NOT NULL,
+    id           bigserial                NOT NULL,
     phone_number text,
-    created_at timestamp with time zone NOT NULL,
-    point bigint NOT NULL,
-    address text,
-    block_status boolean NOT NULL,
+    created_at   timestamp with time zone NOT NULL,
+    point        bigint                   NOT NULL,
+    address      text,
+    block_status boolean                  NOT NULL,
+    hashed_pw    text                     NOT NULL,
+    role         role                     NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.goods
 (
-    id bigserial NOT NULL,
-    name text NOT NULL,
-    price integer NOT NULL,
-    description text NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    view_count integer NOT NULL,
-    image_blob bytea,
-    updated_at timestamp with time zone NOT NULL,
+    id          bigserial                NOT NULL,
+    name        text                     NOT NULL,
+    price       integer                  NOT NULL,
+    description text                     NOT NULL,
+    created_at  timestamp with time zone NOT NULL,
+    view_count  integer                  NOT NULL,
+    image_blob  bytea,
+    updated_at  timestamp with time zone NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.bookmark
 (
-    goods_id bigint NOT NULL,
+    goods_id  bigint NOT NULL,
     member_id bigint NOT NULL,
     FOREIGN KEY (goods_id)
         REFERENCES public.goods (id) MATCH SIMPLE
@@ -43,9 +48,9 @@ CREATE TYPE public.category AS ENUM
 
 CREATE TABLE IF NOT EXISTS public.goods_category
 (
-    goods_id bigserial NOT NULL,
+    goods_id bigint NOT NULL,
     category category,
-    PRIMARY KEY (goods_id),
+    PRIMARY KEY (goods_id, category),
     FOREIGN KEY (goods_id)
         REFERENCES public.goods (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -55,11 +60,11 @@ CREATE TABLE IF NOT EXISTS public.goods_category
 
 CREATE TABLE IF NOT EXISTS public.purchase_history
 (
-    id bigserial NOT NULL,
-    goods_id bigint NOT NULL,
-    member_id bigint NOT NULL,
-    content text NOT NULL,
-    rating smallint NOT NULL,
+    id         bigserial                NOT NULL,
+    goods_id   bigint                   NOT NULL,
+    member_id  bigint                   NOT NULL,
+    content    text                     NOT NULL,
+    rating     smallint                 NOT NULL,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
     PRIMARY KEY (id),
