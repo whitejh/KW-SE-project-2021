34eae2fc6ef847d8ccdcd4f1b5eb6c9128248597
Jung Dong Ho <dongho971220@gmail.com>
2021-05-28 18:09:49 +0900

    member_id 열 추가

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -4,6 +4,7 @@ CREATE TYPE public.role AS ENUM
 CREATE TABLE IF NOT EXISTS public.member
 (
     id           bigserial                NOT NULL,
+    member_id    text                     NOT NULL,
     phone_number text,
     created_at   timestamp with time zone NOT NULL,
     point        bigint                   NOT NULL,
