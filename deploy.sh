d8716b62d401b3817c93378cd274d8d2388a2ff1
Jung Dong Ho <dongho971220@gmail.com>
2021-05-29 00:44:58 +0900

    refactor project structure

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker-compose -p kw up -d --remove-orphans --build
-docker exec -i kw_db_1 psql -U postgres -d postgres -a <src/db/schema.sql
-docker exec -i kw_db_1 psql -U postgres -c "\copy goods from /var/lib/postgresql/dummydata/dummy_goods.csv delimiter ',' NULL as 'NULL' csv header;"
+docker exec -i kw_model_1 psql -U postgres -d postgres -a <deployments/model/schema.sql
+docker exec -i kw_model_1 psql -U postgres -c "\copy goods from /var/lib/postgresql/dummydata/dummy_goods.csv delimiter ',' NULL as 'NULL' csv header;"
