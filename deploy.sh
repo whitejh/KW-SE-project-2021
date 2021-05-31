ad2f1cb29a4208dc0e18d25fa937b2f1f85dbd09
Jung Dong Ho <dongho971220@gmail.com>
2021-05-31 23:02:01 +0900

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 docker-compose -p kw up -d --remove-orphans --build
 docker exec -i kw_db_1 psql -U postgres -d postgres -a <deployments/db/schema.sql
+docker exec -i kw_db_1 psql -U postgres -c "\copy member from /var/lib/postgresql/dummydata/dummy_member.csv delimiter ',' NULL as 'NULL' csv header;"
 docker exec -i kw_db_1 psql -U postgres -c "\copy goods from /var/lib/postgresql/dummydata/dummy_goods.csv delimiter ',' NULL as 'NULL' csv header;"
-docker exec -i kw_db_1 psql -U postgres -c "\copy goods from /var/lib/postgresql/dummydata/dummy_member.csv delimiter ',' NULL as 'NULL' csv header;"
